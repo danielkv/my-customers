77ef87442bde4ede5f43d178358157879b965fc6
Daniel K. Guolo <daniel_kv@hotmail.com>
2021-01-19 23:49:16 -0300

    init server

---
+++ packages/server/src/index.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import cors from 'cors';
+import express, { json } from 'express';
+
+import customers from '../customers.json';
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.get('/', (request, response) => {
+    return response.json({ message: customers.filter((c) => c.id < 500) });
+});
+
+app.listen(3000, () => {
+    console.log('🚀 Server started on http://localhost:3000');
+});
---
+++ packages/server/src/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+// define the router
+const router = Router();
+
+// route the
+router.get('');
