9c213ec8707fe09d785bdea678c9b9e258386d6a
Daniel K. Guolo <daniel_kv@hotmail.com>
2021-01-23 17:10:10 -0300

    fix

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,10 @@
 'use strict';
 
 import 'dotenv/config';
-import 'reflect-metadata';
 
 import express from 'express';
 import { initDataSource } from './init-data-source';
 import { setupServer } from './setup/server.setup';
-import { setupGraphQL } from './setup/graphql.setup';
 
 async function bootstrap() {
     // initial setup
@@ -16,7 +14,6 @@ async function bootstrap() {
     const app = express();
 
     setupServer(app);
-    await setupGraphQL(app);
 
     // start app
     app.listen(3001, () => {
