fa0150819e5e19dbb0acc1fb2e8e8967d4de4aa4
Daniel K. Guolo <daniel_kv@hotmail.com>
2021-01-23 17:27:15 -0300

    fix error handler

--- packages/server/src/error-handler.ts
+++ packages/server/src/error-handler.ts
@@ -1,9 +1,11 @@
-import { Response, Request } from 'express';
+import { Response, Request, NextFunction } from 'express';
 
 /**
  * this function handles all errors for express. It can be more complex depending on the needs
  */
-export function errorHandler(err: Error, req: Request, res: Response) {
+export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    if (!err) return next();
+
     res.status(500);
     return res.render('error', { error: err });
 }
